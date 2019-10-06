5ff85d36ed9ee65c1845cfd69b5008eb618b1e90
lalalaben <lalalaben@outlook.com>
2019-10-06 16:20:13 +0800

    先写个可以摄像的

--- app/src/main/java/com/example/comera/MainActivity.java
+++ app/src/main/java/com/example/comera/MainActivity.java
@@ -13,29 +13,18 @@ import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_VIDEO_CAPTURE = 1;
 
-    private ImageView imageView;
     private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.image);
         videoView = findViewById(R.id.videoView);
 
     }
 
-    public void gotoComera(View view){
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-
-    }
 
     public void gotoVedio(View view){
 
@@ -48,11 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
+
 
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             Uri videoUri = data.getData();
